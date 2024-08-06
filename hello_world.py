401ca9bdcfa6b366597b47082062024e58f8a0c5
AzlaanSiddique <zainuartist@gmail.com>
2024-08-06 15:18:10 +0500

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,41 @@
+#basic printing
+print('hello, world!')
+print("Azlaan")
+
+#printing multiple themes
+print("Azlaan Siddique")
+print("123")
+
+#printing special characters
+print("my name is written below\nAzlaan Siddique")
+print("im using a tab \t character")
+
+#using the sep parameter
+print("world","work","end",sep=',')
+print("3","6","5",sep="-")
+
+#using end parameter
+print("rotten",end=" ")
+print("potato")
+print("1",end="")
+print(2)
+
+#using escape characters
+print("we are so called \"vikings\" from the north")
+print("you can print a backslash\\ by writing a double slash")
+
+#printing numbers and characters together
+age=15
+print(f"Iam {age} years old")
+number=5
+print(f"this is a number {number}")
+
+#basic loop printing
+for x in range (1,6):
+    print(x)
+
+
+
+
+
+
